0696e70662b3d6f823899dc02397d62a07d86d32
1201silver <1201silver@naver.com>
2023-05-16 17:02:04 +0900

    update

---
+++ HW1/UBERStudent20191022.java
@@ -0,0 +1,104 @@
+import java.io.IOException;
+import java.util.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.*;
+import org.apache.hadoop.mapreduce.lib.output.*;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class UBERStudent20191022 {
+
+	public static class UBERMapper extends Mapper<Object, Text, Text, Text>
+	{
+		private final static IntWritable one = new IntWritable(1);
+		private Text uberKey = new Text();
+		private Text uberValue = new Text();
+
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException 
+		{
+			StringTokenizer itr = new StringTokenizer(value.toString(), ",");
+			
+			String region = itr.nextToken();
+			String date = itr.nextToken();
+			
+			String trips = itr.nextToken();
+			String vehicles = itr.nextToken();
+			
+			SimpleDateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+			String[] week = {"MON", "TUE", "WED", "THR", "FRI", "SAT", "SUN"};
+			
+			Date d = new Date();
+			Calendar cal = Calendar.getInstance();
+			try {
+				d = df.parse(date);
+				cal.setTime(d);
+				
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		
+			int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK) - 1;
+			String day = week[dayOfWeek];
+			
+			uberKey.set(region +","+ day);
+			uberValue.set(trips +","+ vehicles);
+			
+			context.write(uberKey, uberValue);		
+		}
+	}
+
+	public static class UBERReducer extends Reducer<Text, Text, Text, Text> 
+	{
+		private Text sumValue = new Text();
+
+		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException 
+		{		
+			int sumT = 0;
+			int sumV = 0;
+			
+			for (Text val : values) 
+			{
+				StringTokenizer itr = new StringTokenizer(val.toString(), ",");
+				int trips = Integer.parseInt(itr.nextToken().trim());
+				int vehicles = Integer.parseInt(itr.nextToken().trim());
+				
+				sumT += trips;
+				sumV += vehicles;
+			
+			}
+			sumValue.set(sumT +","+ sumV);
+			context.write(key, sumValue);
+		}
+	}
+
+	public static void main(String[] args) throws Exception 
+	{
+		Configuration conf = new Configuration();
+		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+		if (otherArgs.length != 2) 
+		{
+			System.err.println("Usage: UBER <in> <out>");
+			System.exit(2);
+		}
+		
+		Job job = new Job(conf, "UBERStudent20191022");
+		
+		job.setJarByClass(UBERStudent20191022.class);
+		job.setMapperClass(UBERMapper.class);
+		job.setReducerClass(UBERReducer.class);
+		
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+		
+		FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+		FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+		FileSystem.get(job.getConfiguration()).delete( new Path(otherArgs[1]), true);
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+}
